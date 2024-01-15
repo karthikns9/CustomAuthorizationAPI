2fc780c1223f689e468b34a779014587d0fba3d1
karthikns999 <karthikns999@gmail.com>
2024-01-16 00:21:41 +0530

    Add project files.

---
+++ CustomAuthorizationAPI/CustomAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http.HttpResults;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.Filters;
+
+//namespace CustomAuthorizationAPI
+//{
+//    public class CustomAuthorizeAttribute : Attribute, IAuthorizationFilter
+//    {
+//        private readonly string _role;
+
+//        public CustomAuthorizeAttribute(string role)
+//        {
+//            _role = role;
+//        }
+
+//        public void OnAuthorization(AuthorizationFilterContext context)
+//        {
+//            if (_role != "admin")
+//            {
+//                context.Result = new BadRequestResult();
+//                return;
+//            }
+
+//            context.Result = new OkResult();
+//        }
+//    }
+//}
