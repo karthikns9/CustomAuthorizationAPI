389a8d212560347863573c3c400420c1e4b0d7db
karthikeyanNS27 <Karthikeyan.NS@integrate.com>
2024-02-07 12:49:30 +0530

    more code

--- CustomAuthorizationAPI/Controllers/WeatherForecastController.cs
+++ CustomAuthorizationAPI/Controllers/WeatherForecastController.cs
@@ -1,5 +1,10 @@
 using CustomAuthorizationService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace CustomAuthorizationAPI.Controllers
 {
@@ -20,7 +25,7 @@ namespace CustomAuthorizationAPI.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        //[MyAuthorize("Admin")]
+        [MyAuthorize("admin")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -30,6 +35,57 @@ namespace CustomAuthorizationAPI.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("get-admin-data")]
+        //[Authorize("RequireAdminRole")]
+        [Authorize(Policy = "RequireAdminRole")]
+        public IActionResult GetAdminData()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            var scopes = new List<string> { "Campaign", "Integrations", "Users" };
+            var permissions = new List<string> { "create", "read", "delete", "update" };
+
+            // Create claims for the user
+            var claims = new List<Claim>
+            {
+            new Claim(ClaimTypes.NameIdentifier, "123"),
+            new Claim(ClaimTypes.Name, "john_doe"),
+            new Claim(ClaimTypes.Role, "admin"),
+            };
+
+            foreach (var scope in scopes)
+            {
+                claims.Add(new Claim("scope", scope));
+            }
+
+            foreach (var permission in permissions)
+            {
+                claims.Add(new Claim("permissions", permission));
+            }
+
+            // Specify the key used to sign the token
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("3272357538782F4125442A472D4B6150645367566B5970337336763979244226"));
+
+            // Create the signing credentials using the key and the algorithm
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // Create the JWT token
+            var token = new JwtSecurityToken(
+                issuer: "https://dev-8uc18ljq3ybzi0w3.us.auth0.com/",
+                audience: "https://glossary.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30), // Token expiration time
+                signingCredentials: creds
+            );
+
+            // Return the token as a string
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
         }
     }
 }
--- CustomAuthorizationAPI/Program.cs
+++ CustomAuthorizationAPI/Program.cs
@@ -1,6 +1,7 @@
 
-
 using CustomAuthorizationService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//}).AddJwtBearer();
+
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddJwtBearer(options =>
+//    {
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuer = true,
+//            ValidateAudience = true,
+//            ValidateIssuerSigningKey = true,
+//            ValidIssuer = "https://dev-8uc18ljq3ybzi0w3.us.auth0.com/",
+//            ValidAudience = "https://glossary.com",
+//            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("3272357538782F4125442A472D4B6150645367566B5970337336763979244226"))
+//        };
+//    });
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("admin"));
+//    // Add more policies as needed for different roles
+//});
+
+builder.Services.AddCustomAuthentication();
+builder.Services.AddCustomAuthorization();
 
 var app = builder.Build();
 
